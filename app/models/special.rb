e7f8fd1025acbf7dbf0deddd4f93173661838cc0
Tony Robertson <tonyr729@gmail.com>
2018-11-26 22:24:48 -0700

    Add Special migration with validation

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,2 +1,5 @@
 class Special < ActiveRecord::Base
+
+  validates_presence_of :name, :image_url, :run_time, :comedian_id
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_025432) do
+ActiveRecord::Schema.define(version: 2018_11_27_041350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_11_27_025432) do
     t.string "name"
     t.integer "age"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.decimal "run_time"
+    t.integer "comedian_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

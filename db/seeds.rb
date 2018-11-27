9d409c67d574289c3d0bdd3cd98b3b769f58a7cc
Tony Robertson <tonyr729@gmail.com>
2018-11-26 19:41:54 -0700

    Add Comedian migration and sample seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,20 @@
+require './app/models/comedian.rb'
 
+Comedian.destroy_all
+
+Comedian.create!(
+  [
+    {
+      name: "Dave Chappelle",
+      age: 45,
+      city: "New York City, NY",
+    },
+    {
+      name: "Louis CK",
+      age: 51,
+      city: "New York City, NY",
+    },
+  ]
+)
+
+p "Created #{Comedian.count} movies"

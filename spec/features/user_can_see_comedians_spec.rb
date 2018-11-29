b7cd914555e2306a505bf7a94bd92552265061fb
Tony Robertson <tonyr729@gmail.com>
2018-11-29 15:44:55 -0700

    Fix feature tests for comedian to be more specific

--- spec/features/user_can_see_comedians_spec.rb
+++ spec/features/user_can_see_comedians_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe 'As a visitor' do
   before(:each) do
     @comedian = Comedian.create(name: "Tony", age: 30, city: "Tahoe")
   end
+
   it 'allows user to see carousel' do
     visit '/comedians'
     expect(page).to have_content("- Laugh Tracks -")
@@ -10,13 +11,18 @@ RSpec.describe 'As a visitor' do
   it 'allows user to see comedians' do
     visit '/comedians'
 
-    within("##{@comedian.id}") do
+    within("#carousel_#{@comedian.id}") do
       expect(page).to have_content(@comedian.name)
+  
+      within("#info_#{@comedian.id}") do
+        expect(page).to have_content(@comedian.age)
+        expect(page).to have_content(@comedian.city)
+      end
     end
 
-    within("##{@comedian.id}") do
-      expect(page).to have_content(@comedian.age)
-      expect(page).to have_content(@comedian.city)
+    within("#img_#{@comedian.id}") do
+      expect(page).to have_content(@comedian.img)
     end
   end
+
 end

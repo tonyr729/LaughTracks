e2327231e7ef03a940becc82c65760b65b092112
Tony Robertson <tonyr729@gmail.com>
2018-12-01 11:55:49 -0700

    Add methods to comedian modle to average_age

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,11 +2,11 @@ class Comedian < ActiveRecord::Base
   has_many :specials
   validates_presence_of :name, :age, :city
 
-    def self.average_age
-      Comedian.average(:age).to_i
-    end
+  def self.average_age
+    Comedian.average(:age).to_i
+  end
 
-      def self.unique_cities
-        Comedian.distinct.pluck(:city)
-      end
+  def self.unique_cities
+    Comedian.distinct.pluck(:city)
+  end
 end
